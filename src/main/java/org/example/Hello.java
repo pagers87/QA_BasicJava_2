9af155ddf04086d49cc7dced4d35b649aa87b17b
Piotr Grzela <pagers87@gmail.com>
2026-04-21 23:24:15 +0200

    poprawki nr 2 w Hello

--- src/main/java/org/example/Hello.java
+++ src/main/java/org/example/Hello.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.print("Hello World!!!");
+        System.out.println("Hello World!!!");
     }
 }
